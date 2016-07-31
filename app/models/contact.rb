0bf617771ba3847f8ae37647342a277d504faaee
Enrique Chenu <enchenu@gmail.com>
2016-07-31 05:54:57 +0000

    Primeiro commit. Todos os arquivos adicionados

---
+++ app/models/contact.rb
@@ -0,0 +1,12 @@
+class Contact < ActiveRecord::Base
+  belongs_to :kind
+  has_one :adrress
+  has_many :phones
+  
+  accepts_nested_attributes_for :adrress
+  accepts_nested_attributes_for :phones, reject_if: :all_blank, allow_destroy: true
+  
+  validates :name, presence: true, length: {minimum: 3}
+  validates :email, presence: true
+
+end
